fbc7c40ac89853d12d9b804baa40381d3a103371
sa20122424 <saloxiddin201224@gmail.com>
2024-08-01 16:12:16 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+//  let ism1 = [];
+// let ism2 = [];
+// let ism3 = [];
+
+// let ism = ["jalol", "o", "ohror", "salom", "saloxiddi"];
+
+for (let i = 0; i < ism.length; i++) {
+  if (ism[i].includes("a")) {
+    ism1.push(ism[i]);
+  } else if (ism[i].includes("o")) {
+    ism2.push(ism[i]);
+  } else if (ism[i].includes("o , a")) {
+    ism3.push(ism[i]);
+  }
+}
+
+console.log("Words with 'a':", ism1);
+console.log("Words with 'o':", ism2);
+console.log("Words with 'a , o':", ism3);
+
